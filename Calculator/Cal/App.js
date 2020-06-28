6856a332c465ee3285faf5bab6aad10b3d67c430
Muzna Ali <Muzna.wajidali@gmail.com>
2020-06-28 15:05:21 +0500

    SaylaniAssing-7

---
+++ Calculator/Cal/App.js
@@ -0,0 +1,18 @@
+function getNumber(num){
+
+  var result = document.getElementById("result");
+  result.value += num;
+}
+function clearResult(){
+  var result = document.getElementById("result");
+  result.value = ""
+}
+function getResult(num){
+  
+  var result = document.getElementById("result");
+  result.value=eval(result.value)}
+
+  function getdot(dot){
+
+    var result = document.getElementById("result");
+    result.value += dot;}
